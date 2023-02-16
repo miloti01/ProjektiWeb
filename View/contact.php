29a6530e3addc4eb8971d684882b530c2dd408b2
Milot Shabani <milot.shabani01@gmail.com>
2023-02-16 22:54:21 +0100

    footer

--- View/contact.php
+++ View/contact.php
@@ -33,58 +33,9 @@
 		</div>
 	</div>
 
-	<footer>
-		<div class="footer-container">
-
-			<div class="footer-logo-container">
-				<div class="footer-logo">ShootShop</div>
-				<span>Copyright 2021</span>
-
-				<div class="footer-social">
-					<a href="#"><i class="fab fa-facebook-f"></i></a>
-					<a href="#"><i class="fab fa-instagram"></i></a>
-					<a href="#"><i class="fab fa-twitter"></i></a>
-				</div>
-			</div>
-
-			<div class="footer-menu">
-				
-				<div class="footer-menu-box">
-					<strong>Rewards</strong>
-					<ul>
-						<li><a href="#">Join Now</a></li>
-						<li><a href="#">Gift Cards</a></li>
-						<li><a href="#">Manage Account</a></li>
-						<li><a href="#">Learn More</a></li>
-						<li><a href="#">Send Us Feedback</a></li>
-					</ul>
-				</div>
-
-				<div class="footer-menu-box">
-					<strong>Get Help</strong>
-					<ul>
-						<li><a href="#">Order Status</a></li>
-						<li><a href="#">Shipping and Delivery</a></li>
-						<li><a href="#">Returns</a></li>
-						<li><a href="#">Payment Options</a></li>
-						<li><a href="#">Contact Us</a></li>
-					</ul>
-				</div>
-
-				<div class="footer-menu-box">
-					<strong>About ShootShop</strong>
-					<ul>
-						<li><a href="#">News</a></li>
-						<li><a href="#">Careers</a></li>
-						<li><a href="#">Investors</a></li>
-						<li><a href="#">Purpose</a></li>
-						<li><a href="#">Sustainability</a></li>
-					</ul>
-				</div>
-			</div>
-
-		</div>
-	</footer>
+	<?php 
+	  include('../Includes/footer.php');
+	?>
 	<span class="copyright">© 2021 ShootShop</span>
 </body>
 </html>
